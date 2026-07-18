77d9213c3192a0f6e29b91cd4da0cce490c309dc
Yusuf Shihata <yusufshihata2006@gmail.com>
2026-07-18 20:19:31 +0300

    some fixes

--- build.py
+++ build.py
@@ -207,6 +207,9 @@ PAGE_TEMPLATE = """<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{title}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{css_path}">
     {extra_css}
 </head>
